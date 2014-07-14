48108fead9248f3b6512ee6a822a367c55ab934e
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-07-14 13:29:15 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+app.configure(function() {
+  app.use(allowCrossDomain);
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser('shhhh, very secret'));
+  app.use(express.bodyParser());
+  app.use(express.session({secret: 'keyboard cat'}));
+});
+
+
+app.get('/matches',  function(req,res){
+  console.log('triggered');
+  res.status(200).sendfile(__dirname + '/mockData/test.txt');
+});
+
+
+app.get('/events', function(req, res){
+  res.status(200).sendfile(__dirname + '/mockData/test.txt');
+}); 
+  
+
+
+var port = process.env.PORT || 8080;
+
+app.listen(port);
+
+console.log('Server now listening on port ' + port);
